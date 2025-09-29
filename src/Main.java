301940da1090f3862493a67bc3b2c6ae2ee58661
andrey <andrey.sn41@gmail.com>
2025-09-29 17:40:46 +0500

    Рекурсия

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 //        String name = deck.getName(id);
 //        System.out.println(sum + "\t" + name);
 //        if(sum > 21) {System.out.println("Вы проиграли!!");}
-        sum = fun(deck, sum, input);
+//        sum = fun(deck, sum, input);
 
 //        System.out.println("Ещё?");
 //        input.nextLine();
@@ -24,7 +24,7 @@ public class Main {
 //        System.out.println(sum + "\t" + name);
 //        if(sum > 21) {System.out.println("Вы проиграли!!");}
 
-        sum = fun(deck, sum, input);
+//        sum = fun(deck, sum, input);
 
 //        System.out.println("Ещё?");
 //        input.nextLine();
@@ -35,7 +35,7 @@ public class Main {
 //        System.out.println(sum + "\t" + name);
 //        if(sum > 21) {System.out.println("Вы проиграли!!");}
 
-        sum = fun(deck, sum, input);
+//        sum = fun(deck, sum, input);
 
 //        System.out.println("Ещё?");
 //        input.nextLine();
@@ -50,19 +50,32 @@ public class Main {
 //        System.out.println("Ещё?");
 //        input.nextLine();
 
-        sum = fun(deck, sum, input);
+        fun(deck, sum, input);
     }
 
-    private static int fun(Deck deck, int sum, Scanner input) {
+    private static void fun(Deck deck, int sum, Scanner input) {
         int id = deck.get();
         sum = sum + deck.getPoint(id);
         String name = deck.getName(id);
         System.out.println(sum + "\t" + name);
-        if(sum > 21) {System.out.println("Вы проиграли!!");}
 
-        System.out.println("Ещё?");
-        input.nextLine();
+        if(sum > 21) {
+            System.out.println("Вы проиграли!!");
+//            return;
+        }else  if(sum == 21) {
+            System.out.println("Вы выиграли!!");
+//            return;
+        }else {
+            System.out.println("Ещё? У - да, N - нет");
+            String s = input.nextLine();
 
-        return sum;
+            char s1 = s.toLowerCase().trim().charAt(0);
+
+            if(s1 == 'y') {
+                fun( deck, sum, input);
+            }
+//            return;
+        }
+//        return;
     }
 }
