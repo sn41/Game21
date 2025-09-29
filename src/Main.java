3b6bacc2e58df5bbf1d9747cf81a907d9e22faa2
andrey <andrey.sn41@gmail.com>
2025-09-29 18:55:47 +0500

    Рекурсия

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Объявляется
         Deck deck = new Deck();
         Scanner input = new Scanner(System.in);
 
         int sum = 0;
-
+        char s1 = ' ';
         //вытягиваем карту
-//        int id = deck.get();
-//        sum = sum + deck.getPoint(id);
-//        String name = deck.getName(id);
-//        System.out.println(sum + "\t" + name);
-//        if(sum > 21) {System.out.println("Вы проиграли!!");}
-//        sum = fun(deck, sum, input);
-
-//        System.out.println("Ещё?");
-//        input.nextLine();
-//
-//        id = deck.get();
-//        sum = sum + deck.getPoint(id);
-//        name = deck.getName(id);
-//        System.out.println(sum + "\t" + name);
-//        if(sum > 21) {System.out.println("Вы проиграли!!");}
-
-//        sum = fun(deck, sum, input);
-
-//        System.out.println("Ещё?");
-//        input.nextLine();
-//
-//        id = deck.get();
-//        sum = sum + deck.getPoint(id);
-//        name = deck.getName(id);
-//        System.out.println(sum + "\t" + name);
-//        if(sum > 21) {System.out.println("Вы проиграли!!");}
-
-//        sum = fun(deck, sum, input);
-
-//        System.out.println("Ещё?");
-//        input.nextLine();
-//
-//
-//        id = deck.get();
-//        sum = sum + deck.getPoint(id);
-//        name = deck.getName(id);
-//        System.out.println(sum + "\t" + name);
-//        if(sum > 21) {System.out.println("Вы проиграли!!");}
-//
-//        System.out.println("Ещё?");
-//        input.nextLine();
-
-        fun(deck, sum, input);
+        do{
+            int id = deck.get();
+            sum = sum + deck.getPoint(id);
+            String name = deck.getName(id);
+            System.out.println(sum + "\t" + name);
+            fun(deck, sum, input);
+            System.out.println("Играем? У - да, N - нет");
+            String s = input.nextLine();
+             s1 = s.toLowerCase().trim().charAt(0);
+        }while (s1 == 'y') ;
     }
 
     private static void fun(Deck deck, int sum, Scanner input) {
@@ -61,21 +29,15 @@ public class Main {
 
         if(sum > 21) {
             System.out.println("Вы проиграли!!");
-//            return;
         }else  if(sum == 21) {
             System.out.println("Вы выиграли!!");
-//            return;
         }else {
             System.out.println("Ещё? У - да, N - нет");
             String s = input.nextLine();
-
             char s1 = s.toLowerCase().trim().charAt(0);
-
             if(s1 == 'y') {
                 fun( deck, sum, input);
             }
-//            return;
         }
-//        return;
     }
 }
