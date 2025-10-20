3cd7e8fec167f75c64b478d422509ac67f0fc12e
andrey <andrey.sn41@gmail.com>
2025-10-20 17:57:04 +0500

    с3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +6,25 @@ public class Main {
 //        System.out.println(deck1.getName(2) + "\t" + deck1.getPoint(2));
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Игрок 1!");
+        int sum = game(deck1, input, "Игрок 1");
+        int sum2 = game(deck1, input, "Игрок 2");
+
+        if (sum > 21 && sum2 > 21) {
+            System.out.println("Оба проиграли");
+        } else if (sum2 > 21) {
+            System.out.println("Первый выиграл");
+        }else if (sum > 21) {
+            System.out.println("Второй выиграл");
+        }if (sum >sum2) {
+            System.out.println("Первый выиграл");
+        }
+
+
+//        int point = Deck1.getName(2);
+    }
+
+    private static int game(Deck1 deck1, Scanner input, String title) {
+        System.out.println(title);
         int sum = 0;
         int id1 = deck1.getId();
         sum += Deck1.getPoint(id1);
@@ -18,8 +34,7 @@ public class Main {
         sum += Deck1.getPoint(id1);
         System.out.println(Deck1.getName(id1));
 
-        boolean flag = true;
-        while (flag) {
+        while (true) {
             System.out.println();
             System.out.println("Общее = " + sum);
             System.out.println("Ещё? (Да/Нет)");
@@ -28,48 +43,16 @@ public class Main {
                 id1 = deck1.getId();
                 sum += Deck1.getPoint(id1);
                 System.out.println(Deck1.getName(id1));
-                if(sum>20){
+                if (sum > 20) {
                     break;
                 }
-            }else {
+            } else {
                 break;
             }
         }
 
         System.out.println("Очки первого игрока = " + sum);
-
-        System.out.println("Игрок 2!");
-        int sum2 = 0;
-        id1 = deck1.getId();
-        sum += Deck1.getPoint(id1);
-        System.out.println(Deck1.getName(id1));
-
-        id1 = deck1.getId();
-        sum += Deck1.getPoint(id1);
-        System.out.println(Deck1.getName(id1));
-
-         flag = true;
-        while (flag) {
-            System.out.println();
-            System.out.println("Общее = " + sum2);
-            System.out.println("Ещё? (Да/Нет)");
-            String answer = input.nextLine().toLowerCase();
-            if (answer.equals("да")) {
-                id1 = deck1.getId();
-                sum2 += Deck1.getPoint(id1);
-                System.out.println(Deck1.getName(id1));
-                if(sum2>20){
-                    break;
-                }
-            }else {
-                break;
-            }
-        }
-        System.out.println("Очки первого игрока = " + sum2);
-
-
-
-//        int point = Deck1.getName(2);
+        return sum;
     }
 
 }
